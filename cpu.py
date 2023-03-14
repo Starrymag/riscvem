9c3d6c0abd04f0ded2adae24725277e3c38394a6
Starrymag <starrymag101@gmail.com>
2023-03-14 13:55:48 +0300

    add more instructions

--- cpu.py
+++ cpu.py
@@ -3,7 +3,6 @@ import glob
 from elftools.elf.elffile import ELFFile
 from enum import Enum
 
-regfile = [0]*33
 PC = 32
 
 # 64K at 0x80000000
@@ -24,6 +23,9 @@ class Regfile:
         self.regs[key] = value & 0xFFFFFFFF
 
 
+regfile = Regfile()
+
+
 class Ops(Enum):
     LUI    = 0b0110111  # U type
     AUIPC  = 0b0010111  # U type
@@ -38,7 +40,7 @@ class Ops(Enum):
     OP     = 0b0110011  # R type
 
     MISC  = 0b0001111   # 
-    ECALL  = EBREAK = 0b1110011  # also I type
+    SYSTEM = 0b1110011  # also I type
 
 
 class Funct3(Enum):
@@ -51,6 +53,18 @@ class Funct3(Enum):
     SRLI = SRL = SRAI = SRA = 0b101
     ORI = OR = 0b110
     ANDI = AND = 0b111
+
+    BEQ = 0b000
+    BNE = 0b001
+    BLT = 0b100
+    BGE = 0b101
+    BLTU = 0b110
+    BGEU = 0b111
+
+
+class Funct7(Enum):
+    ADD = XOR = OR = AND = SLL = SRL = SLT = SLTU = 0b0000000
+    SUB = SRA = 0b0100000
 
 
 # write segment
@@ -67,7 +81,8 @@ def r32(addr):
     global memory
     addr -= 0x80000000
     # check addres validity
-    assert addr >= 0 and addr < len(memory)
+    if addr < 0 or addr > len(memory) - 4:
+        raise Exception("read out of bound: 0x%x" % addr)
     return struct.unpack("<I", memory[addr:addr+4])[0]
 
 
@@ -82,6 +97,7 @@ def dump():
 
 
 def sign_extend(num, length):
+    # check first bit
     if num >> (length - 1) == 1:
         return - ((1 << length) - num)
     else:
@@ -98,39 +114,77 @@ def step():
     # Instruction decode
     opcode = Ops(gibi(6, 0))
     print("%x %8x %r" % (regfile[PC], ins, opcode))
+
     if opcode == Ops.JAL:
         # J-type instruction
         rd = gibi(11, 7)
-        assert rd == 0
         # calculate offset
-        imm_j = gibi(31, 30) << 20 | gibi(30, 21) << 1 | gibi(21, 20) << 11 | \
-            gibi(20, 12) << 12
+        imm_j = sign_extend(((gibi(32, 31) << 20) | (gibi(30, 21) << 1) |
+                            (gibi(21, 20) << 11) | (gibi(19, 12) << 12)), 21)
+        regfile[rd] = regfile[PC] + 4
         regfile[PC] += imm_j
         return True
+
+    if opcode == Ops.JALR:
+        # I-type instruction
+        rd = gibi(11, 7)
+        rs1 = gibi(19, 15)
+        imm_i = sign_extend(gibi(31, 20), 12)
+        offset = regfile[rs1] + imm_i
+        # set lsb to zero
+        offset = 2 * (offset // 2)
+        regfile[rd] = regfile[PC] + 4
+        regfile[PC] = offset
+        print(offset)
+        return True
+
     elif opcode == Ops.IMM:
         # I-type instruction
         funct3 = Funct3(gibi(14, 12))
         rd = gibi(11, 7)
         rs1 = gibi(19, 15)
-        # TODO Use sign extend
-        imm_i = gibi(31, 20)
+        imm_i = sign_extend(gibi(31, 20), 12)
         if funct3 == Funct3.ADDI:
             regfile[rd] = regfile[rs1] + imm_i
+        elif funct3 == Funct3.SLLI:
+            regfile[rd] = regfile[rs1] << imm_i
         else:
             raise Exception("write %r" % funct3)
         regfile[PC] += 4
         return True
+
     elif opcode == Ops.AUIPC:
         # U-type of instruction
         rd = gibi(11, 7)
         imm_u = gibi(31, 12)
-        print("Dist: x%x IMM: %x" % (rd, imm_u))
+        # PAY ATTENTION to bit shift
+        imm_u = imm_u << 12
+        # print("Dist: x%x IMM: %x" % (rd, imm_u))
         regfile[rd] = regfile[PC] + imm_u
+        regfile[PC] += 4
+        return True
+
+    elif opcode == Ops.SYSTEM:
+        pass
+
+    elif opcode == Ops.OP:
+        rd = gibi(11, 7)
+        rs1 = gibi(19, 15)
+        rs2 = gibi(24, 20)
+        funct3 = Funct3(gibi(14, 12))
+        funct7 = Funct7(gibi(31, 25))
+        if funct3 == Funct3.ADD and funct7 == Funct7.ADD:
+            regfile[rd] = regfile[rs1] + regfile[rs2]
+        else:
+            raise Exception("wirte funct3 %r, funct7 %r" % (funct3, funct7))
+    else:
+        dump()
+        raise Exception("unknown opcode: %r" % opcode)
 
     # execute
     # write the result
-    dump()
-    return False
+    regfile[PC] += 4
+    return True
 
 
 def out_section(filename, elf):
